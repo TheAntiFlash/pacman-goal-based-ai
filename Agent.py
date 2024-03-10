393789d83c7e8496babbc35c3f57788e971e7c63
TheAntiFlash <yahya.bin.naveed@gmail.com>
2024-03-10 21:30:57 +0500

    Added comments to files

--- Agent.py
+++ Agent.py
@@ -2,6 +2,7 @@ from Environment import Environment, State, stateCount
 from os import system, name
 
 
+# Function that uses os to clear screen.
 def clear():
     # for windows
     if name == 'nt':
@@ -15,25 +16,40 @@ def clear():
 class Agent(Environment):
     def __init__(self):
         super().__init__()
+
+        # Initial values of pacman agent.
         self.row = 0
         self.col = 0
+
+        # value of how many moves till cherry power up is over. Initially zero until cherry is picked up.
         self.cherry_power_up_cooldown = 0
-        # self.game = self.grid_to_game()
+        # Placing pacman emoji on (0,0)
         self.game[self.row][self.col] = 'ᗧ'
 
+        # Goal is to eat all food_pallet. this will get the current count of food that will be
+        # tracked throughout the session.
         foodLeft = stateCount[State.Food_Pallet]
+        # This is the previous state pacman was at. initialized to current state for now. Keep track of this to avoid
+        # being stuck in a loop
         prev = (0, 0)
+
+        # Session will run until all food_pallets are eaten.
         while foodLeft > 0:
             clear()
             if self.cherry_power_up_cooldown > 0:
                 print("Cherry Power Up!!!", self.cherry_power_up_cooldown, "s LEFT!")
             self.show_game()
+
+            # Get all positions pacman can move to.
             options = self.options_to_move()
 
-            priority = {option: 0 for option in options}  # 4 for food, 3 for cherry, 2 for when there is ghost and
+            # initialize a dictionary for all options, setting default priority to 0.
+            # 4 for food, 3 for cherry, 2 for when there is ghost and
             # cherry is eaten. 1 for empty. 0 for ghost.
-            options_left = len(options)
+            priority = {option: 0 for option in options}
+
             input("Press any key to continue...\n")
+            # Loops through all options and assigns it a priority based on its state.
             for option in options:
 
                 if self.grid[option[0]][option[1]] == State.Food_Pallet.value:
@@ -47,15 +63,25 @@ class Agent(Environment):
 
                 elif self.grid[option[0]][option[1]] == State.Empty.value:
                     priority[option] = 1
-                options_left -= 1
 
+            # Gets the current max priority available amongst the options to move.
             maxPriority = max(priority.values())
+
+            # Ensuring the max priority isn't 0 (in case of ghost and no powerup)
             if maxPriority > 0:
+
+                # checks if we ignore the option with max priority which was the previous state,
+                # do we have any other options?
+                # if the if condition is true we move to the previous state.
+                # else move to the second state with max priority
                 if next((key for key, value in priority.items() if value == maxPriority and key != prev), None) is None:
                     option = next((key for key, value in priority.items() if value == maxPriority), None)
                 else:
-                    option = next((key for key, value in priority.items() if value == maxPriority and key != prev), None)
+                    option = next((key for key, value in priority.items() if value == maxPriority and key != prev),
+                                  None)
                 prev = (self.row, self.col)
+
+                # Moving to state with max priority keeping in mind previous state as well.
                 if maxPriority == 4:
                     self.move(option)
                     foodLeft -= 1
@@ -81,12 +107,15 @@ class Agent(Environment):
         self.show_game()
         print("Mission Accomplished!!!")
 
+    # Returns the coordinates adjacent to pacman.
     def options_to_move(self):
         offsets = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         adjacent_coordinates = [(self.row + dr, self.col + dc) for dr, dc in offsets if
                                 0 <= self.row + dr <= 3 and 0 <= self.col + dc <= 3]
 
         return adjacent_coordinates
+
+    # Moves pacman
 
     def move(self, new_pos):
         self.row = new_pos[0]
--- Environment.py
+++ Environment.py
@@ -2,6 +2,7 @@ from enum import Enum
 from random import shuffle
 
 
+# State for each cell in game grid.
 class State(Enum):
     Empty = 0
     Ghost = 1
@@ -9,6 +10,7 @@ class State(Enum):
     Food_Pallet = 3
 
 
+# number of such states for each randomly generated game environment.
 stateCount = {
     State.Ghost: 4,
     State.Cherry: 5,
@@ -16,6 +18,7 @@ stateCount = {
 
 }
 
+# Converting State to emoji to display in game.
 stateToEmoji = {
     State.Ghost.value: '👻',
     State.Cherry.value: '🍒',
@@ -26,17 +29,26 @@ stateToEmoji = {
 
 class Environment:
     def __init__(self):
+        # Generates a list of each state * how many of each state should be in any game
         random_data = ([State.Ghost.value] * stateCount[State.Ghost]
                        + [State.Cherry.value] * stateCount[State.Cherry]
                        + [State.Food_Pallet.value] * stateCount[State.Food_Pallet])
+
+        # Shuffles the random data to set the environment for the game.
         shuffle(random_data)
-        random_data.insert(0, 0)
+
+        # Inserts the empty state at index 0 for where pacman will be placed
+        random_data.insert(0, State.Empty.value)
+        # Converts 16 length array to 4x4 array
         self.grid = [random_data[i:i + 4] for i in range(0, len(random_data), 4)]
+        # Converts the grid of values 0-3 to their emoji counterparts to display.
         self.game = self.grid_to_game()
 
+    # Converts the grid to game view
     def grid_to_game(self):
         return [[str(stateToEmoji[cell]) for cell in row] for row in self.grid]
 
+    # Displays the game to console.
     def show_game(self):
         for row in self.game:
             print(row)
